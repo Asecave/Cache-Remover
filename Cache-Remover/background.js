a6e5741fa6c3242b18ad2a0de36e78545334c4d4
Asecave <39377667+Asecave@users.noreply.github.com>
2022-11-20 13:16:30 +0100

    moved on github

---
+++ Cache-Remover/background.js
@@ -0,0 +1,44 @@
+
+let defaultSettings = {
+  "websites": []
+};
+
+function onError(e) {
+  console.error(e);
+}
+
+function checkStoredSettings(storedSettings) {
+  if (!storedSettings.websites) {
+    browser.storage.local.set(defaultSettings);
+  }
+}
+
+function forget() {
+
+  console.log("Forgetting...");
+
+  const dataTypes = {
+    cache: true,
+    serviceWorkers: true,
+  };
+
+  options = {
+    hostnames: ["soundcloud.com"],
+  };
+
+  browser.browsingData.remove(options, dataTypes);
+}
+
+const gettingStoredSettings = browser.storage.local.get();
+gettingStoredSettings.then(checkStoredSettings, onError);
+
+browser.browserAction.onClicked.addListener(() => {
+  const gettingStoredSettings = browser.storage.local.get();
+  //gettingStoredSettings.then(forget, onError);
+});
+
+browser.webRequest.onBeforeRequest.addListener(
+  listener,
+  {urls: ["https://example.com/*"], types: ["main_frame"]},
+  ["blocking"]
+);
